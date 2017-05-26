066afcc39cc90387d54f2762fb52e54d276fceb4
BearRan <648070256@qq.com>
2017-05-26 08:36:02 +0800

    Initial Commit

---
+++ CRTextFieldAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CRTextFieldAnimation
+//
+//  Created by Bear on 2017/5/26.
+//  Copyright © 2017年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
